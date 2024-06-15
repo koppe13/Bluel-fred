53214e7bdd764d90acc37257fdc8abc9bd5a3d3f
koppe13 <phenixairsoft2@gmail.com>
2024-06-15 22:56:01 +0200

    maj du  script
    modification pour appel du json et integration html

--- FrontEnd/assets/Scripts/script.js
+++ FrontEnd/assets/Scripts/script.js
@@ -1,33 +1,20 @@
-//let balisemur = document.querySelectorAll('.gallery figure')
-//console.log(balisemur)
+let balisemur = document.querySelectorAll('.gallery')
 
-//for (let i = 0; i < balisemur.length; i++) {
-//    console.log(balisemur[i])
-//}
+document.addEventListener("DOMContentLoaded", gojson)
+async function gojson() {    
+    const responses = await fetch("http://localhost:5678/api/works")
+    const works = await responses.json()
+    //console.log(works)
+    //const imageProjet = document.createElement("img")
+   
 
-//function demarrage () {
-   //let photo = document.getElementById('un')
-    //let murImage = document.createElement("img")
-
-   // photo.src = imageProjet[1]
-
-    
-//document.figure.appendChild(image1)
-//}
-
-
-async function getWorks() 
-{
-    try 
-    {
-        const response = await fetch("http://localhost:5678/api/works");
-        const works = await response.json();
-
-     console.log(works);
-    } 
-    catch(error)
-    {
-        console.error(error);
+    for ( i in works ){
+        //console.log(works[i])
+        balisemur.innerHTML += '<img src="' + works[i].imageUrl + '">'
     }
+
 }
-getWorks();
+
+
+
+
